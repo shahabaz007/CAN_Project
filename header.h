617dff6900a397f8ccccf2eaaed22d914160cc40
SHAIK SHAHABAZ <74087239+shahabaz007@users.noreply.github.com>
2025-08-06 12:38:41 +0530

    header.h

--- header.h
+++ header.h
@@ -1,7 +1,5 @@
 #define sw1 14
-
 #define sw2 15
-
 #define sw3 16
 typedef struct CAN2
 {
@@ -15,4 +13,5 @@ void can_init(void);
 void can_tx(c2 m1);
 void can_rx(c2 *m1);
 void can_delay(unsigned int);
+
 
